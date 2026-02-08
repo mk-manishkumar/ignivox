1a7a9807d67582beb6f9f70cfda3f74e4c3b7604
Manish Kumar <manish.login01@gmail.com>
2026-02-08 22:42:27 +0530

    feat : Add foundational UI components and initial landing page structure with product listings.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import FeaturedProducts from "@/components/landing-page/featured-products"
 import HeroSection from "@/components/landing-page/hero-section"
+import RecentlyLaunchedProducts from "@/components/landing-page/recently-launched-products"
 
 const page = () => {
   return (
     <div>
       <HeroSection />
+      <FeaturedProducts/>
+      <RecentlyLaunchedProducts/>
     </div>
   )
 }
